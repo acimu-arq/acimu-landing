8fe0a267a29da5aef028b4dbf643ae6ddc84fe89
Pablo Guallichico <pablo.guallichico@outlook.com>
2025-12-12 23:32:32 -0500

    feat: Add FAQ data and update contact page

---
+++ src/data/faq.ts
@@ -0,0 +1,52 @@
+export default [
+  {
+    question:
+      '¿Cuánto tiempo toma realizar los planos y cuál es el tiempo estimado de aprobación en el municipio?',
+    answer:
+      'La elaboración de los planos para proyectos grandes se realiza en un plazo de aproximadamente 15 días laborables. Para proyectos más pequeños, el tiempo de elaboración puede ser menor, ajustándose a la complejidad del trabajo. Respecto a la aprobación municipal, los tiempos dependen de cada municipio, pero generalmente el proceso suele demorar entre tres y cuatro meses.',
+  },
+  {
+    question:
+      '¿Qué información y documentos debo entregar para que se inicie el diseño arquitectónico desde cero?',
+    answer: '',
+  },
+  {
+    question: '¿Trabajan solo diseño arquitectónico o también construcción?',
+    answer:
+      'Trabajamos en ambas áreas, tanto el servicio de diseño arquitectónico como el de construcción. Contamos con una cuadrilla altamente capacitada que se encarga de ejecutar las obras, garantizando calidad y cumplimiento en cada proyecto.',
+  },
+  {
+    question: '¿Se encargan de tramitar permisos y licencias?',
+    answer:
+      'Sí, en ACIMU nos encargamos de tramitar los permisos y licencias necesarios. Como estudio buscamos que los proyectos de nuestros clientes se ejecuten desde el inicio con toda la documentación en regla.',
+  },
+  {
+    question: '¿Quien se encarga de supervisar, una vez comenzada la obra?',
+    answer:
+      'La supervisión de la obra la realizamos nosotros mismos. Contamos con la capacidad y la experiencia necesarias para asegurar que cada etapa del proyecto se ejecute correctamente y cumpla con los estándares de calidad.',
+  },
+  {
+    question:
+      '¿El permiso de construcción garantiza que el municipio no paralice ni multe la obra ?',
+    answer:
+      'Contar con el permiso de construcción reduce significativamente el riesgo de que el municipio paralice o multe la obra, ya que acredita que el proyecto cumple con los requisitos legales. Sin embargo, es importante mantener toda la documentación actualizada y ejecutar la obra conforme a lo aprobado para evitar inconvenientes.',
+  },
+  {
+    question:
+      '¿Los costo del diseño arquitectónico pueden cambiar dependiendo de algún factor?',
+    answer:
+      'No, los precios son fijos. Disponemos de un amplio catalogo de servicios cada uno con su rango de precio optimo. Cada obra se le asigna el precio mas justo dependiendo del caso.',
+  },
+  {
+    question:
+      '¿El estudio ofrece asesoría para cumplir con las normativas municipales y evitar contratiempos durante la construcción?',
+    answer:
+      'Todos los trabajos que tenga el fin de obtener la licencia de construcción cumple con la normativa vigente en cada municipio e institución (NEC, RTAU, INEN, Bomberos, entre otros.) En el caso de diseños que por decisión del cliente no desee aprobar por ninguna institución también serán cumplirán con la normativa, siendo solo modificados bajo la petición del cliente.',
+  },
+  {
+    question:
+      'Si tengo varias dudas y necesito asesoramiento de su estudio, ¿tiene algún costo?',
+    answer:
+      'No, nuestro asesoramiento inicial no tiene ningún costo. Estamos completamente disponibles para resolver tus dudas y brindarte la orientación que necesites sin compromiso. Sin embargo, si el caso requiere una visita técnica en el sitio, esta tendría un costo de 35 dólares.',
+  },
+];
